928c3580ae7345521923a5b00c6ba237c903e491
IamAkhil123 <90715959+IamAkhil123@users.noreply.github.com>
2021-12-10 14:54:00 -0800

    udpate

--- task1.py
+++ task1.py
@@ -1,21 +1,13 @@
 #! python3
+ 
+""
+print("enter a number")
+input()str
 
-"""
-Have the user input a number. 
-Determine if the number is larger than 100 
-If it is, the output should read "The number is larger than 100" 
-(2 points)
-Inputs:
-number
-Outputs:
-"The number is larger than 100"
-"The number is smaller than 100"
-"The number is 100"
-
-Example:
-Enter a number: 100
-The number is 100
-
-Enter a number: 102
-The number is larger than 100
-"""
+if () < 100:
+ print("Your number is less than 100")
+if () > 100:
+ print("Your number is greater than 100")
+if () == 100:
+ print("Your number is equal to 100")
+""
