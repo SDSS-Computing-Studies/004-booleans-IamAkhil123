082b12a6566f6e2546338797c6ab0b0ec594dac2
IamAkhil123 <90715959+IamAkhil123@users.noreply.github.com>
2021-12-14 13:59:45 -0800

    update

--- task1.py
+++ task1.py
@@ -10,7 +10,7 @@ if int(x) < 100:
 if int(x) == 100:
  print("The number is 100")
 """
-Open the file called task1.dpy \
+Open the file called task1.py \
 Have the user input a number. \
 Determine if the number is larger than 100 \
 If it is, the output should read "The number is larger than 100" \
